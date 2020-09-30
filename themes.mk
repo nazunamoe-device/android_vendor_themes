ffa41a697332fd6c65b29ec10f2c02c21e1faae0
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-09-30 11:20:36 +0700

    themes:import from vendor/cherish/themes

---
+++ themes.mk
@@ -0,0 +1,145 @@
+# Themes
+PRODUCT_PACKAGES += \
+    ClearSpringSystemOverlay \
+    ClearSpringSystemUIOverlay \
+    PitchBlackSystemOverlay \
+    PitchBlackSystemUIOverlay \
+    SolarizedDarkSystemOverlay \
+    SolarizedDarkSystemUIOverlay \
+    BakedGreenSystemOverlay \
+    BakedGreenSystemUIOverlay \
+    ChocoXSystemOverlay \
+    ChocoXSystemUIOverlay \
+    MaterialOceanSystemOverlay \
+    MaterialOceanSystemUIOverlay \
+    DarkGreySystemOverlay \
+    DarkGreySystemUIOverlay 
+
+# QS header styles
+PRODUCT_PACKAGES += \
+    QSHeaderBlack \
+    QSHeaderGrey \
+    QSHeaderLightGrey \
+    QSHeaderAccent \
+    QSHeaderTransparent
+	
+# Switch themes
+PRODUCT_PACKAGES += \
+    SwitchMD2 \
+    SwitchOnePlus \
+    SwitchStock \
+	Contained \
+    Retro \
+    Stockish \
+    Narrow
+	
+# Accents
+PRODUCT_PACKAGES += \
+    BrownAccent \
+    TealAccent \
+    BlackAccent \
+    YellowAccent \
+    RedAccent \
+    PurpleAccent \
+    PinkAccent \
+    OrangeAccent \
+    GreenAccent \
+    WhiteAccent \
+    CyanAccent \
+    ObfusBleu \
+    NotImpPurple \
+    Holillusion \
+    MoveMint \
+    AlmostProBlue \
+    BubblegumPink \
+    FrenchBleu \
+    Stock \
+    ManiaAmber \
+    DrownedAquaBlue \
+    BoucheRed \
+    DreamyPurple \
+    GrapesPurple \
+    SpookedPurple \
+    MisleadingRed \
+    FSRGrey \
+    HeirloomBleu \
+    LunaBlue \
+    WarmthOrange \
+    NaturedGreen \
+    ColdBleu \
+    DiffDayGreen \
+    DuskPurple \
+    BurningRed \
+    SimilarDawnRed \
+    SpoofyGreen \
+    NewHouseOrange \
+    LimedGreen \
+    SunsetOrange \
+    UiGradientsDIMIGO
+
+# QS tile styles
+PRODUCT_PACKAGES += \
+    QSTileDefault \
+    QSTileCircleTrim \
+    QSTileCircleDualTone \
+    QSTileCircleGradient \
+    QSTileCookie \
+    QSTileDottedCircle \
+    QSTileDualToneCircle \
+    QSTileInk \
+    QSTileInkdrop \
+    QSTileMountain \
+    QSTileNinja \
+    QSTileOreo \
+    QSTileOreoCircleTrim \
+    QSTileOreoSquircleTrim \
+    QSTilePokesign \
+    QSTileSquaremedo \
+    QSTileSquircle \
+    QStileBadgetwo \
+    QSTileSquircleTrim \
+    QSTileTeardrop \
+    QStileDiamond \
+    QStileHexagon \
+    QStileSquare \
+    QSTileWavey \
+    QStileGear \
+    QStileStar \
+    QStileBadge \
+    QStileNeonlike \
+    QStileOOS \
+    QStileTriangles \
+    QStileDivided \
+    QStileCosmos \
+    QStileRoundedRect
+    
+# Notification themes
+PRODUCT_PACKAGES += \
+    NotificationBlackTheme \
+    NotificationDarkTheme \
+    NotificationLightTheme
+	
+# Brightness sliders Styles
+PRODUCT_PACKAGES += \
+    BrightnessSliderDefault \
+    BrightnessSliderDaniel \
+    BrightnessSliderMemeMini \
+    BrightnessSliderMemeRound \
+    BrightnessSliderMemeRoundStroke \
+    BrightnessSliderMemeStroke
+	
+# NavBar themes
+PRODUCT_PACKAGES += \
+ 	StockAsusOverlay \
+    NavbarAsusOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarTecnoCamonOverlay
+	
+# UI Styles
+PRODUCT_PACKAGES += \
+    UiStyleDefault \
+    UiStyleRoundLarge \
+    UiStyleRoundMedium \
+    UiStyleNoCornerRadius \
+    UiStyleRectangle \
