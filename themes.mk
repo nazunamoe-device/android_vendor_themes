650f3ebdd75e5a439db3ac09b71e779a1dda7ed9
Dyneteve <sound0020@gmail.com>
2020-10-04 23:22:32 +0200

    overlay: packages: Set the styles and wallpapers package
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- themes.mk
+++ themes.mk
@@ -1,3 +1,9 @@
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
+
+# Allow overlays to be excluded from enforcing RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
+
 # Themes
 PRODUCT_PACKAGES += \
     ClearSpringSystemOverlay \
